f5e096949bf9e700027c3ca88112a587bf4a0c86
anchietajunior <santosjr87@gmail.com>
2020-01-16 17:00:28 -0300

    Add front-end application with ReactJS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
@@ -12,6 +13,7 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
